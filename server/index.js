68ff4ef7e01442c238dc8d441c95b72905bcdb00
TingzhouJia <joe.tzjia@gmail.com>
2020-06-11 22:59:15 -0400

    :rocket: revise api

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express=require('express')
 const cors=require('cors')
 const bodyParser=require('body-parser')
-
+const Inflection=require('inflection')
 const app=express()
 app.use(cors())
 app.use(bodyParser.json())                                                                                                             
-app.use('/admin/api',require('./routes/admin'))
+app.use('/admin/api/rest/:resource',async(req,res,next)=>{
+    const modelName=Inflection.classify(req.params.resource)
+    req.Model=require(`../../models/${modelName}`)
+    next()
+},require('./routes/admin'))
 require('./plugins/mongo')(app)
 app.listen(3000,()=>{
     console.log('http://localhost:3000')
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const router = express.Router(); 
-const Category=require('../../models/category')
-router.post('/categories', (req,res)=>{  
-    const model= Category.create(req.body).catch(e=>console.log(e))
+
+router.post('/', (req,res)=>{  
+    const model= req.Model.create(req.body).catch(e=>console.log(e))
     res.send(model)
 })
 
-router.get('/categories',async (req,res)=>{
-    const items=await Category.find().limit(10)
+router.get('/',async (req,res)=>{
+    let queryOptions={}
+    if(req.Model.modelName=='Category'){
+        queryOptions.populate='parent'
+    }
+    const items=await req.Model.find().setOptions({
+       queryOptions
+    }).limit(10)
     res.send(items)
 })
 
-router.get('/categories/:id',async (req,res)=>{
+router.delete('/:id',async(req,res)=>{
+    await Category.findByIdAndDelete(req.params.id)
+    res.status(200).json({message:'success'})
+})
+router.get('/:id',async (req,res)=>{
     const model=await Category.findById(req.params.id);
     res.send(model)
 })
 
-router.put('/categories/:id',async (req,res)=>{
+router.put('/:id',async (req,res)=>{
     const model=Category.findByIdAndUpdate(req.params.id,req.body)
     res.send(model)
 })
